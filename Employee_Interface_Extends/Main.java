1c478e307d032caf6846dfbdb4b29388e5df868a
Mai Vu Bao Long <109711387+MVBL112004@users.noreply.github.com>
2024-03-08 07:26:30 +0700

    Bai tap java

--- Employee_Interface_Extends/Main.java
+++ Employee_Interface_Extends/Main.java
@@ -9,17 +9,14 @@ public class Main {
         String name = scanner.nextLine();
         System.out.print("2.Luong theo gio: ");
         int paymentPerHour = scanner.nextInt();
-
         int employeeType;
         do {
             System.out.print("3.Chon loai nhan vien (1 - Part-time, 2 - Full-time): ");
             employeeType = scanner.nextInt();
-
             if (employeeType != 1 && employeeType != 2) {
                 System.out.println("ERROR: Lua chon khong hop le! vui long chon 1 hoac 2.");
             }
         } while (employeeType != 1 && employeeType != 2);
-
         Employee employee;
         if (employeeType == 1) {
             System.out.println("4.So gio lam viec: ");
@@ -28,12 +25,10 @@ public class Main {
         } else {
             employee = new FullTimeEmployee(name, paymentPerHour);
         }
-
         System.out.println("--Xuat thong tin nhan vien--");
         System.out.println("1.Ten nhan vien: " + employee.getName());
         System.out.println("2.Luong theo gio: " + employee.getPaymentPerHour());
         System.out.println("3.Luong thang: " + employee.calculateSalary());
-
         scanner.close();
 
     }
